27c166ab4971799e5e2e9eff5123a4eefd80dc2c
Ravindisha Wijayalath <61857732+RavindishaWi@users.noreply.github.com>
2023-04-08 10:33:36 +0530

    Create additional components
    created components for prediction, evaluation and image/video selection

---
+++ frontend/oasst-ped-det/src/app/img-vid-selection/img-vid-selection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-img-vid-selection',
+  templateUrl: './img-vid-selection.component.html',
+  styleUrls: ['./img-vid-selection.component.css']
+})
+export class ImgVidSelectionComponent {
+
+}
