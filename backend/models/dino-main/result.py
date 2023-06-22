cb898adae52f7809bf3ad3504866dd531d5bcfc3
Ravindisha Wijayalath <61857732+RavindishaWi@users.noreply.github.com>
2023-06-22 21:29:08 +0530

    Update result.py

--- backend/models/dino-main/result.py
+++ backend/models/dino-main/result.py
@@ -1,15 +1,13 @@
 import subprocess
 
 def visualize_attention(image_path, model_weight_path, working_directory):
-    python_interpreter_path = '../../../venv/Scripts/python.exe'  # update this path
+    python_interpreter_path = '../../../venv/Scripts/python.exe'
     script_path = 'visualize_attention.py'
 
     command = [python_interpreter_path, script_path, '--image_path', image_path, '--pretrained_weights',
                model_weight_path]
     subprocess.run(command, check=True, cwd=working_directory)
 
-
-# Example usage
 image_path = 'images/foggy.jpg'
 model_weight_path = 'checkpoint.pth'
 working_directory = '.'
